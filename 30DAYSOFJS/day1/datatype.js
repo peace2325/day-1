bff71c3ef1ee83726c5da76c44a0b05635cdccd7
peace2325 <110161162+peace2325@users.noreply.github.com>
2022-10-16 16:17:39 -0700

    Update datatype.js

--- 30DAYSOFJS/day1/datatype.js
+++ 30DAYSOFJS/day1/datatype.js
@@ -1,5 +1,5 @@
-console.log(typeof 'Asabeneh') // 
-console.log(typeof 5) // 
-console.log(typeof 'true') // 
-console.log(typeof 'null') // 
-console.log(typeof 'undefined') // 
+console.log(typeof 'Asabeneh') 
+console.log(typeof 5) 
+console.log(typeof true) 
+console.log(typeof null) 
+console.log(typeof undefined) 
