32d4426f9d08697b58b502944961d421cfdaf893
peace2325 <110161162+peace2325@users.noreply.github.com>
2022-10-16 16:06:40 -0700

    Update variable.js

--- 30DAYSOFJS/day1/variable.js
+++ 30DAYSOFJS/day1/variable.js
@@ -2,9 +2,10 @@ let firstName = "AINA"
 let isMarried = 'true'
 let lastName 
 let emptyValue = null
+console.log(firstName, isMarried, lastName,emptyValue)
 
 
-
+// Variable without assigning value
 let firstName 
 let lastName
 let age
@@ -12,16 +13,17 @@ let isMarried
 
 
 
-('single line')
+// single line
 let firstName = 'aina', lastName = 'grace', maritalStatus = 'true', country = 'uk', age = '20'
 
-('multiple line')
+// multiple line
 let firstName = 'aina'
 let lastName = 'grace'
-let maritalStatus = 'true'
+let maritalStatus = true
 let country = 'uk'
-let age = '20'
-
-
-let myAge = '25'
-let yourAge = '30'
+let age = 20
+console.log(firstName, lastName,maritalStatus, country, age)
+// Declare two variable myAge and yourAge
+let myAge = 25
+let yourAge = 30
+console.log(`I am ${myAge} years old you are ${30} years old`)
